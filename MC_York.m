f08e23f4b24f723f62ffa7391f080442973f48d2
Byron Adams <39592382+baadams@users.noreply.github.com>
2020-07-24 13:41:44 +0100

    Update MC_York.m

--- MC_York.m
+++ MC_York.m
@@ -1,4 +1,4 @@
-function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
+function [coeff,n,MSWD,MSWD_std,coeff_err,x_plot,y_plot] = MC_York(b,x,y,x_err,y_err,varargin)
 %-------------------------------------------------------------------------%
 % Function written by Byron A. Adams - Updated: 11 Apr 2019
 %-------------------------------------------------------------------------%
@@ -42,10 +42,6 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
 % Optional inputs:
 % num - number of monte carlo simulations
 % x_max - maximum value of x to use in calculations
-% fig_num - this is the figure number to be used
-% colour - color of plotted outputs. [r g b] format where rgb are 0-1  
-% print - flag that sets whether resulting regression should be printed to
-%         screen 'y' for yes, 'n' for no. yes is the default
 %
 % Outputs:
 % n - the best-fit power law exponent
@@ -54,42 +50,37 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
 % MSWD_std - 1 sigma uncertainty on the MSWD
 %
 %-------------------------------------------------------------------------%
-% tashi delek
+% tashi delek!
 %-------------------------------------------------------------------------%
-%
+
 % parse inputs
 	p = inputParser;         
 	p.FunctionName = 'MC_York';
-    %
+    
 	addRequired(p,'b',@(x) isnumeric(x));
 	addRequired(p,'x',@(x) isnumeric(x));
     addRequired(p,'y',@(x) isnumeric(x));
     addRequired(p,'x_err',@(x) isnumeric(x));
     addRequired(p,'y_err',@(x) isnumeric(x));
-    %
+    
     addParameter(p,'num',1e6,@(x) isnumeric(x));
 	addParameter(p,'x_max',max(x),@(x) isscalar(x));
-    addParameter(p,'fig_num',1,@(x) isscalar(x));
-    addParameter(p,'colour',[0 0 0],@(x) isnumeric(x));
-    addParameter(p,'print','y',@(x) ischar(x));
-    %
+    
 	parse(p,b,x,y,x_err,y_err,varargin{:});
 	b = p.Results.b;
 	x = p.Results.x;
     y = p.Results.y;
     x_err = p.Results.x_err;
     y_err = p.Results.y_err;
-    %
+    
     num = p.Results.num;
 	x_max = p.Results.x_max;
-    fig_num = p.Results.fig_num;
-    colour = p.Results.colour;
-    print = p.Results.print;
-%
+
 % initialize matricies
     x_box = zeros(num,length(x));
-    y_box = zeros(num,length(x)); 
-%    
+    y_box = zeros(num,length(x));
+    a_values = zeros(1,num);
+   
 % make normal distributions of x and y based on 1 sigma uncertainties
     for k = 1:length(x)
         rng('shuffle');
@@ -97,16 +88,11 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
         rng('shuffle');
         y_box(:,k) = y_err(k)*randn(1,num) + y(k);
     end
-%
+
 % reshape the data and remove negative values
 	new_x = reshape(x_box,[num*length(x),1]);
 	new_y = reshape(y_box,[num*length(x),1]);
-	rmv_x = find(new_x < 0);
-	rmv_y = find(new_y < 0);
-	rmv = [rmv_x;rmv_y];
-	new_x(rmv) = [];
-	new_y(rmv) = [];
-%
+	
 % if the exponent is is not known (b has length > 1), start by calculating
 % the best-fit exponent, which is the slope of the data once log-
 % transformed. if b is fixed (b has length = 1) then only calculate the
@@ -118,28 +104,28 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
         	tol = 1e-6;
         % set maximum number of iterations
         	runs = 1e6;
-        %
+        
         % monte carlo loop. calculate regression parameters for each
         % simulation
         for j = 1:num
             % intialize chi_sq with some large number
                 chi_sq = 1e6;
-            %
+            
             % make an array of x and y points from the distributions
                 xi = transpose(x_box(j,:));
                 yi = transpose(y_box(j,:));
-            %
+            
             % create equal weighting values for x and y (e.g. make them 1)
             	omega_x = (ones(length(x),1));
             	omega_y = (ones(length(x),1));
-            %
+            
             % linearize the randomly selected data
             	xi = log(xi);
             	yi = log(yi);
-            %
+            
             % intialize b with an approximation for slope
                 b = (yi(1)-yi(length(yi)))/(xi(1)-xi(length(xi)));
-            %
+            
             % find the slope (b)and intercept (a). loop iterates b until
             % chi_square < tol
             for i = 1:runs
@@ -158,7 +144,7 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
                     chi_sq = chi_sq_new;
                 end
             end
-            %
+            
             % remove simulations containing negative slopes. this can be
             % created by sampling distributions beyond 2 standard
             % deviations.
@@ -169,60 +155,52 @@ function [coeff,n,MSWD,MSWD_std] = MC_York(b,x,y,x_err,y_err,varargin)
                 end
         end
         % find mean b
-            b = mean(b_all,'omitnan');
-        %
+            b = median(b_all,'omitnan');
+        
         % linearize the entire synthetic mote carlo dataset
         	trans_x = log(new_x);
         	trans_y = log(new_y);
-        %    
+          
         % create equal weighting the entire synthetic mote carlo dataset
         	omega_x = ones(length(trans_x),1);
         	omega_y = ones(length(trans_x),1);
-        %
+        
         % solve for a given the solution for b above
         	Wi = (omega_x.*omega_y)./((omega_y.*(b^2)) + omega_x);
         	X_bar = sum(Wi.*trans_x)./sum(Wi);
         	Y_bar = sum(Wi.*trans_y)./sum(Wi);
             a = Y_bar - X_bar*b;
+            coeff_err = NaN;
     else
         % this else loop solves for a if b is fixed
-        % invert the exponent
             b = 1/b;
-        % linearize the data
-        	trans_x = log(new_x);
-        	trans_y = log(new_y);
-        %
-        % create equal weighting values for x and y
-        	omega_x = ones(length(trans_x),1);
-        	omega_y = ones(length(trans_x),1);
-        %
-        % because the value of b is a provided input in this case, use only
-        % the essential equations from York to solve for a
-            Wi = (omega_x.*omega_y)./((omega_y.*(b^2)) + omega_x);
-            X_bar = sum(Wi.*trans_x)/sum(Wi);
-            Y_bar = sum(Wi.*trans_y)/sum(Wi);
-            a = Y_bar - X_bar*b;
+            
+        for j = 1:num
+            % make an array of x and y points from the distributions
+                xi = transpose(x_box(j,:));
+                yi = transpose(y_box(j,:));
+                      
+            % linearize the randomly selected data
+            	xi = log(xi);
+            	yi = log(yi);
+            
+            % find the
+                a_values(j) = mean(yi) - mean(xi)*b;
+        end
+        a = real(median(a_values));
+        a_std = real(std(a_values));
+        coeff_err = exp(a_std);
 	end
-%
+        
+% calculate the MSWD of the best-fit parameters
+    coeff = real(exp(a));
+    n = real(1/b);
+	C = 1/coeff^n;
+	chi_sq = sum(((x - C.*(y.^n)).^2)./((x_err.^2) + (C.*n.*y.^(n - 1)).^2.*(y_err.^2)));
+	MSWD = chi_sq/(length(x) - 2);
+	MSWD_std = sqrt(2/(length(x) - 2));
+    
 % based on the best b (slope) and a (intercept) values, create synthetic
 % x and y data for plotting the best-fit curve
 	x_plot = 0:1:x_max;
-	y_plot = exp(a).*x_plot.^b;
-%        
-% write data for output
-	coeff = exp(a);
-    n = 1/b;
-%
-% calculate the MSWD of the best-fit parameters
-	C = 1/coeff^n;
-	chi_sq = sum(((x - C.*(y.^b)).^2)./((x_err.^2) + (C.*b.*y.^(b - 1)).^2.*(y_err.^2)));
-	MSWD = chi_sq/(length(x) - 2);
-	MSWD_std = sqrt(2/(length(x) - 2));
-%
-% plot best-fit curve and parameters
-    if print == 'y'
-        figure(fig_num)
-        hold on
-        line(x_plot,y_plot,'Color',colour,'LineWidth',2.5,'DisplayName','LSE')
-    end
-%
+	y_plot = real(exp(a).*x_plot.^b);
